63c555288e6f4ac2373daf8f13c05387c2dc7c4b
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-11-21 10:56:50 +0530

    display employees in employeelist component

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [
+    {
+      id: 'one',
+      name: 'Maneesha',
+      email: 'Maneesha@gmail.com',
+      phone: '0763565740',
+      department: 'FT',
+    },
+    {
+      id: 'two',
+      name: 'Dilkushi',
+      email: 'Dilkushi@gmail.com',
+      phone: '07635640',
+      department: 'FT',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
